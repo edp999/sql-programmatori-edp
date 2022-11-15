b3e293e6ae857f01d7fde786341f2c38971a27e1
edp999 <edipietro23@gmail.com>
2022-11-15 17:30:11 +0100

    17.30

--- esercizio.sql
+++ esercizio.sql
@@ -6,12 +6,17 @@ where a.id=pa.id and pa.linguaggio='Java' and pa.anno>2000
 /* 2. Calcolare il nome e la categoria dei programmatori che 
 sono stati autori di almeno un programma scritto in un linguaggio diverso
  da Python, ordinando il risultato rispetto al nome dei programmatori. */
-select pe.nome, pe.categoria
+select pe.nome, pe.categoria, pa.linguaggio
 from autore a ,programma pa, programmatore pe
 where a.id=pa.id and pe.codice = a.codice and pa.linguaggio<>'Python'
 order by pe.nome
 /* 3. Per ogni programmatore di categoria 10, calcolare il suo codice e
-testetstetst l'anno in cui ha scritto il primo programma in un linguaggio diverso da Java. */
+l'anno in cui ha scritto il primo programma in un linguaggio diverso da Java. */
+select pe.codice, min(pa.anno)
+from programmatore pe, programma pa, autore a
+where a.id=pa.id and pe.codice = a.codice 
+    and pe.categoria=10 and pa.linguaggio<>'java'
+group by pe.codice
 
 /* 4. Calcolare le coppie dei codici di programmatori che sono stati
  coautori di almeno un programma scritto in Python. */
